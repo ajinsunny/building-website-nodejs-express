9d15df29b00f84ac04cf7042495cecaff6c8cfdf
a <ajin.sunny@gmail.com>
2021-06-03 08:19:02 -0700

    add all files

---
+++ server.js
@@ -0,0 +1,22 @@
+const { request, response } = require('express');
+const express = require('express');
+const path = require('path');
+const { ppid } = require('process');
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, './static')));
+
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (request, response) => {
+  response.sendFile(path.join(__dirname, './static/speakers.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}!`);
+});
